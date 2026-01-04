4f1d9e76b74cc85a4fd8e74a9cb5e32c6171f477
khushnawaj <nawajkhush86@gmail.com>
2026-01-04 22:57:31 +0530

    updated themeSlice for persist them mode

--- src/store/themeSlice.js
+++ src/store/themeSlice.js
@@ -1,15 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialTheme =
+  localStorage.getItem("theme") || "dark";
+
+if (initialTheme === "dark") {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
 const themeSlice = createSlice({
   name: "theme",
-  initialState: { mode: "dark" },
+  initialState: { mode: initialTheme },
   reducers: {
     toggleTheme: (state) => {
       state.mode = state.mode === "dark" ? "light" : "dark";
-    },
-  },
+
+      localStorage.setItem("theme", state.mode);
+
+      if (state.mode === "dark") {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+    }
+  }
 });
 
 export const { toggleTheme } = themeSlice.actions;
 export default themeSlice.reducer;
-
